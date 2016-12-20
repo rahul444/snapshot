1bcf17e0fbe99bb1082d82bc61d3772738d13f26
Rahul <rahul444@hotmail.com>
2016-12-19 16:56:19 -0800

    frontend form linked with backend

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var google = require('google-trends-api');
-
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
     console.log("home page");
     res.sendFile(path.join(__dirname + '/index.html'));
+});
 
+function findTrend(inp) {
+
+}
+
+app.get('/search', function(req, res) {
+    console.log('searching for: ' + req.query.name);
+    
     var timePeriod = {
         type: 'hour',
         value: 4
     }
 
-    google.trendData('liverpool', timePeriod)
+    google.trendData(req.query.name, timePeriod)
     .then(function(results) {
         console.log(JSON.stringify(results));
+        res.send("Trend Found:\n" + JSON.stringify(results));
         findTrend(results);
     })
     .catch(function(err) {
         console.log(err);
     });
-});
-
-function findTrend(inp) {
-    
-}
-
-app.get("/search", function(req, res) {
-    console.log(req.body.name);
-    res.send("you sent");
 });
 
 app.listen(port, function() {
