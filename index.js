99d8ced978d2a8b57447adbc510e39cacda0e7f7
Rahul <rahul444@hotmail.com>
2016-12-22 21:57:59 -0800

    minor syntax fixes

--- index.js
+++ index.js
@@ -10,17 +10,18 @@ var db;
 
 app.get("/", function(req, res) {
     console.log("home page");
-    // updateDatabase("James Harden", "Dunk", "2th Quarter Game 6 Second Round");
+    updateDatabase("James Harden", "Game Winner", "Game 7");
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-function updateDatabase(name, type, time){
-  db.collection('players').update({},{$addToSet : {[name] : {"desc":type, "time":time}}},false,true, (err, result) => {
-      if (err) return console.log(err)
-      console.log('saved to database')
-  });
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+function updateDatabase(name, type, time) {
+    console.log("in update");
+    db.collection('players').update({},{$addToSet : {[name] : {"desc":type, "time":time}}},false,true, (err, result) => {
+        if (err) 
+            return console.log(err);
+        console.log('saved to database');
+    });
+};
 
 // function findTrend(inp) {
 //     var total = 0;
@@ -34,12 +35,12 @@ function updateDatabase(name, type, time){
 // }
 
 MongoClient.connect('mongodb://ratham:rocketssuck13@ds143608.mlab.com:43608/snapshot-player-log', (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  app.listen(4000, () => {
-    console.log('listening on 4000')
-  })
-})
+    if (err) return console.log(err)
+    db = database
+    app.listen(4000, () => {
+        console.log('listening on 4000')
+    });
+});
 
 // app.get('/search', function(req, res) {
 //     console.log('searching for: ' + req.query.name);
